1360e5eccda0db88b572d62d55e1788cc8fbad68
onnivaahtera <onni.vaahtera@gmail.com>
2024-12-10 12:47:19 +0200

    airplane animation

--- frontend/scripts/gameHandler.js
+++ frontend/scripts/gameHandler.js
@@ -1,28 +1,28 @@
 import {
-  createHeader,
-  createMap,
-  createSideMenu,
-  drawPointsOnMap,
+	createHeader,
+	createMap,
+	createSideMenu,
+	drawPointsOnMap,
 } from "./gameHandlerElements.js";
 import { playerHandler } from "./playerDataHandler.js";
 
 const gameHandler = () => {
-  startGame();
-  playerHandler.renderData();
-  playerHandler.renderAirports();
-  playerHandler.sendAirplanes();
+	startGame();
+	playerHandler.renderData();
+	playerHandler.renderAirports();
+	playerHandler.sendAirplanes();
 };
 
 const startGame = () => {
-  /* Luo pelin elementit */
-  const gameContainer = document.createElement("main");
-  gameContainer.classList.add("game-container", "background-gradient");
+	/* Luo pelin elementit */
+	const gameContainer = document.createElement("main");
+	gameContainer.classList.add("game-container", "background-gradient");
 
-  gameContainer.appendChild(createHeader());
-  gameContainer.appendChild(createSideMenu());
-  gameContainer.appendChild(createMap());
+	gameContainer.appendChild(createHeader());
+	gameContainer.appendChild(createSideMenu());
+	gameContainer.appendChild(createMap());
 
-  document.body.appendChild(gameContainer);
+	document.body.appendChild(gameContainer);
 };
 
 export default gameHandler;
--- frontend/scripts/gameHandlerElements.js
+++ frontend/scripts/gameHandlerElements.js
@@ -2,96 +2,97 @@ import { ALL_AIRPORTS } from "./preloadAssets.js";
 import { displayShop } from "./shopHandler.js";
 
 const createHeader = () => {
-  // Header missä näkyy pelaajan tilanne (raha etc)
-  const gameHeader = document.createElement("header");
-  gameHeader.classList.add("game-header");
+	// Header missä näkyy pelaajan tilanne (raha etc)
+	const gameHeader = document.createElement("header");
+	gameHeader.classList.add("game-header");
 
-  // Pelaajan raha
-  const playerMoneySpan = document.createElement("span");
-  playerMoneySpan.classList.add("game-header-money");
-  playerMoneySpan.id = "player-money-counter";
+	// Pelaajan raha
+	const playerMoneySpan = document.createElement("span");
+	playerMoneySpan.classList.add("game-header-money");
+	playerMoneySpan.id = "player-money-counter";
 
-  // Pelaajan co2 päästöt
-  const playerCo2Span = document.createElement("span");
-  playerCo2Span.classList.add("game-header-co2");
-  playerCo2Span.id = "player-co2-counter";
+	// Pelaajan co2 päästöt
+	const playerCo2Span = document.createElement("span");
+	playerCo2Span.classList.add("game-header-co2");
+	playerCo2Span.id = "player-co2-counter";
 
-  // Lisää elementit headeriin
-  gameHeader.appendChild(playerMoneySpan);
-  gameHeader.appendChild(playerCo2Span);
+	// Lisää elementit headeriin
+	gameHeader.appendChild(playerMoneySpan);
+	gameHeader.appendChild(playerCo2Span);
 
-  return gameHeader;
+	return gameHeader;
 };
 
 const createSideMenu = () => {
-  const sideMenu = document.createElement("div");
-  sideMenu.classList.add("game-sidemenu");
-  sideMenu.textContent = "Select airport";
+	const sideMenu = document.createElement("div");
+	sideMenu.classList.add("game-sidemenu");
+	sideMenu.textContent = "Select airport";
 
-  return sideMenu;
+	return sideMenu;
 };
 
 const createMap = () => {
-  const gameMapContainer = document.createElement("div");
-  gameMapContainer.classList.add("game-map");
+	const gameMapContainer = document.createElement("div");
+	gameMapContainer.classList.add("game-map");
 
-  const gameMap = document.createElement("div");
+	const gameMap = document.createElement("div");
 
-  const gameMapImage = document.createElement("img");
-  gameMapImage.src = "/assets/world_map.png";
+	const gameMapImage = document.createElement("img");
+	gameMapImage.classList.add("game-map-image");
+	gameMapImage.src = "/assets/world_map.png";
 
-  drawPointsOnMap(
-    gameMap,
-    Object.entries(ALL_AIRPORTS).map((i) => [i[1].lat, i[1].lng, i[0]]),
-  );
+	drawPointsOnMap(
+		gameMap,
+		Object.entries(ALL_AIRPORTS).map((i) => [i[1].lat, i[1].lng, i[0]])
+	);
 
-  gameMap.appendChild(gameMapImage);
-  gameMapContainer.appendChild(gameMap);
+	gameMap.appendChild(gameMapImage);
+	gameMapContainer.appendChild(gameMap);
 
-  return gameMapContainer;
+	return gameMapContainer;
 };
 
 const drawPointsOnMap = (mapWrapper, coordinates) => {
-  if (coordinates.length == 0)
-    return console.error("Coordinates is array empty");
-  for (const x of coordinates) {
-    /* Tarkistus jos jokin kordinaatti puuttuu etc */
-    if (
-      (!x[0] && typeof x[0] !== "number") ||
-      (!x[1] && typeof x[1] !== "number") ||
-      (!x[2] && typeof x[2] !== "string")
-    ) {
-      console.error(
-        `Invalid coordinates X: "${x[0]}", Y: "${x[1]}", Airport: "${x[2]}"`,
-      );
-      continue;
-    }
+	if (coordinates.length == 0)
+		return console.error("Coordinates is array empty");
+	for (const x of coordinates) {
+		/* Tarkistus jos jokin kordinaatti puuttuu etc */
+		if (
+			(!x[0] && typeof x[0] !== "number") ||
+			(!x[1] && typeof x[1] !== "number") ||
+			(!x[2] && typeof x[2] !== "string")
+		) {
+			console.error(
+				`Invalid coordinates X: "${x[0]}", Y: "${x[1]}", Airport: "${x[2]}"`
+			);
+			continue;
+		}
 
-    const mapButton = document.createElement("button");
-    mapButton.classList.add("game-map-button");
+		const mapButton = document.createElement("button");
+		mapButton.classList.add("game-map-button");
 
-    mapButton.addEventListener("click", () => displayShop(x[2]));
-    mapButton.id = x[2];
+		mapButton.addEventListener("click", () => displayShop(x[2]));
+		mapButton.id = x[2];
 
-    const point = coordinatesToPercent(x[0], x[1]);
-    mapButton.style.left = `calc(${point.x} - 1em)`;
-    mapButton.style.top = `calc(${point.y} - 1em)`;
+		const point = coordinatesToPercent(x[0], x[1]);
+		mapButton.style.left = `calc(${point.x} - 1em)`;
+		mapButton.style.top = `calc(${point.y} - 1em)`;
 
-    mapWrapper.appendChild(mapButton);
-  }
+		mapWrapper.appendChild(mapButton);
+	}
 };
 
 const coordinatesToPercent = (lat, lng) => {
-  /* vähä oudot arvot ku tää ei oo mikään yleinen kartta projektio. Nämä on silti suhtkoht OK */
-  const x = (50 + 100 / (290 / lng)) * Math.cos(34 * (Math.PI / 180)) + "%";
-  const y = (50 - 100 / (160 / lat)) / Math.cos(29 * (Math.PI / 180)) + "%";
-  return { x, y };
+	/* vähä oudot arvot ku tää ei oo mikään yleinen kartta projektio. Nämä on silti suhtkoht OK */
+	const x = (50 + 100 / (290 / lng)) * Math.cos(34 * (Math.PI / 180)) + "%";
+	const y = (50 - 100 / (160 / lat)) / Math.cos(29 * (Math.PI / 180)) + "%";
+	return { x, y };
 };
 
 export {
-  createHeader,
-  createSideMenu,
-  createMap,
-  drawPointsOnMap,
-  coordinatesToPercent,
+	createHeader,
+	createSideMenu,
+	createMap,
+	drawPointsOnMap,
+	coordinatesToPercent,
 };
